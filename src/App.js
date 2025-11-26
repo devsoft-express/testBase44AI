070e6202e2e3c220faff27c6c80e5f358a4ac9e8
Iulian Ionel <69301159+devsoft-express@users.noreply.github.com>
2025-11-26 21:54:46 +0100

    Update src/App.js via AI

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Library from './Library';
+
+function App() {
+  return (
+    <div>
+      <h1>Library Store</h1>
+      <Library />
+    </div>
+  );
+}
+
+export default App;
